4d9b98082f21842995bf04ed532d77e1b172f166
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-06-15 11:25:40 +0300

    deleted codebase

--- eccommerce-product-page/src/App.js
+++ eccommerce-product-page/src/App.js
@@ -1,12 +1,8 @@
-import Header from './components/Header';
-import MainPage from './components/MainPage';
-import "./css/style.css"
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <MainPage />
+      
     </div>
   );
 }
