d8cdf95cd7c0449561f9f8ca1bae2b4cdc184924
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-06-15 11:31:22 +0300

    new codebase

---
+++ eccommerce-product-page/src/components/ProductImages/LargeImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LargeImage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LargeImage
---
+++ eccommerce-product-page/src/components/ProductImages/SmallImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SmallImage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SmallImage
